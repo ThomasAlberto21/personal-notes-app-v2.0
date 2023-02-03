90fe18a9c581f20de5e1bed124ccef831db96a45
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-03 23:20:50 +0700

    Finish

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import AddNote from './pages/AddNote';
 import ArchiveNote from './pages/ArchiveNote';
+import NotFoundPage from './pages/NotFoundPage';
 import HomePageWrapper from './pages/HomePage';
-import { Route, Routes } from 'react-router-dom';
 import DetailNoteWrapper from './pages/DetailNote';
+import { Route, Routes } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/addnote' element={<AddNote />} />
         <Route path='/notes/:id' element={<DetailNoteWrapper />} />
         <Route path='/archivednotes' element={<ArchiveNote />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <p className='notes-list-empty'>Page Not Found</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
